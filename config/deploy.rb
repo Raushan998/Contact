0ba8e46db89530c5018f0aa9c34d600ee05e58da
Raushan998 <raushan.raman23011999@gmail.com>
2024-04-03 05:33:03 +0530

    added configuration for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock "~> 3.18.1"
 
 set :application, 'contact'
 set :repo_url, 'git@github.com:Raushan998/Contact.git'
-set :branch, :master
+set :branch, :main
 set :deploy_to, '/home/deploy/contact'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
