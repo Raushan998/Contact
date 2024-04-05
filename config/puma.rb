499dd521a6dabb85c4fd97177b3d116f4a3f8860
Raushan998 <raushan.raman23011999@gmail.com>
2024-04-05 07:55:47 +0530

    removed puma files from deploy.rb files

--- config/puma.rb
+++ config/puma.rb
@@ -7,9 +7,6 @@
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
-require 'puma/daemon'
-bind 'tcp://0.0.0.0:3000'
-daemonize
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
