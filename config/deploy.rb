749078c19b4b8d0b1775df3ad82f626accaaeaad
Raushan998 <raushan.raman23011999@gmail.com>
2024-04-05 09:05:29 +0530

    fixed the deploy.rb file

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,11 +14,11 @@ set :rvm_ruby_version, 'ruby-3.3.0'
 
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/contact_puma_production.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/contact_puma_production.sock"
-set :puma_conf, "#{shared_path}/puma_contact_production.rb"
-set :puma_access_log, "#{shared_path}/log/contact_puma_production_access.log"
-set :puma_error_log, "#{shared_path}/log/contact_puma_production_error.log"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_access.log"
+set :puma_error_log, "#{shared_path}/log/puma_error.log"
 set :puma_role, :app
 set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
 set :puma_threads, [0, 8]
